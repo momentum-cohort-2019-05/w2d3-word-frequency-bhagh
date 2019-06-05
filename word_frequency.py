178a94c92b47972c1b040371e6769c26f775df36
bhagh <haghb1@gmail.com>
2019-06-05 18:32:52 -0400

    List generated but not in alpha

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,7 @@
+import re
+from collections import Counter
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +11,43 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    raw_string = open(file).read()
+    clean_string = (re.sub("[^a-zA-Z0-9]+", ' ', raw_string))
+    clean_string = clean_string.lower()
+    querywords = clean_string.split()
+    word_list = ""
+
+    for word in querywords:
+        if word not in STOP_WORDS:
+            word_list += str(word)+" "
+    
+    word_list = word_list.split() 
+
+    master_list = {}
+
+    for word in word_list:
+        if master_list.get(word) == None:
+            master_list[word] = 1
+        else:
+            master_list[word] += 1
+
+
+
+    def second_item(seq):
+        return seq[1]
+    
+    def first_item(seq):
+        return seq[0]
+
+
+    def top_ten(quantities):
+        quantities = sorted(quantities.items(), key=second_item, reverse=True)[0:10]
+        
+        for word, qty in quantities:
+                print(word, qty)
+        
+    print(top_ten(master_list))
+
 
 
 if __name__ == "__main__":
@@ -25,3 +65,4 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
